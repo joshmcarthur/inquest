aec8cf9e974ad6be8848f9814b8ccd7b1ab7baaf
Josh McArthur <joshua.mcarthur@gmail.com>
2013-03-23 09:55:35 +1300

    Write user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.new(
+                :email => 'tester@example.com', 
+                :password => 'password1', 
+                :password_confirmation => 'password1'
+              )
+  }
+  context "with valid attributes" do
+    it "should be valid" do
+      user.should be_valid
+    end
+  end
+
+  context "with missing attributes" do
+    before do
+      user.password = ""
+    end
+    
+    it "should not be valid" do
+      user.should_not be_valid
+    end
+  end
+
 end
