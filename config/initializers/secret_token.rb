e69e682f0382a7e8dddbf1ac3bb67781b563524e
Josh McArthur <joshua.mcarthur@gmail.com>
2013-03-22 20:12:19 +1300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inquest::Application.config.secret_token = '14826fc577088676c9e9c28ea4d5b692b2f7aa5d8bb8512928547e8d22fe82f46b24065c13c480f0619113d807e7d8d0f456bc8bd08c8d0d71f2f84f2db1eb69'
