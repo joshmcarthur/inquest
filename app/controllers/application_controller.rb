33c415b8973f711b0cc413b4f525420678f02f2a
Josh McArthur <joshua.mcarthur@gmail.com>
2013-03-22 22:22:55 +1300

    Add login and reset password pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  layout :layout_by_resource
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "login"
+    else
+      "application"
+    end
+  end
 end
