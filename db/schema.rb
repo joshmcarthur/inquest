6ed04b1d9da49bc4b67a08a3c8728724694b2955
Charles Peach <charles@3months.com>
2013-03-22 22:20:10 +1300

    Associate Answers with Questions

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130322084745) do
+ActiveRecord::Schema.define(:version => 20130322090304) do
 
   create_table "answers", :force => true do |t|
-    t.text     "content",                       :null => false
-    t.boolean  "approved",   :default => false, :null => false
-    t.datetime "created_at",                    :null => false
-    t.datetime "updated_at",                    :null => false
+    t.text     "content",                        :null => false
+    t.boolean  "approved",    :default => false, :null => false
+    t.datetime "created_at",                     :null => false
+    t.datetime "updated_at",                     :null => false
+    t.integer  "question_id",                    :null => false
   end
 
   create_table "questions", :force => true do |t|
