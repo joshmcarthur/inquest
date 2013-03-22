4aa6e5934e90851e8395bac05732a24d76b8ca7f
Charles Peach <charles@3months.com>
2013-03-22 22:19:51 +1300

    Question has many answers

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,8 @@
 class Question < ActiveRecord::Base
   include ActiveModel::ForbiddenAttributesProtection
 
+  has_many :answers
+
   validates :title, :presence => true
   validates :content, :presence => true
-
 end
