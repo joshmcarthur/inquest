03ecb61d47d12e8b99d20f6acda1053842cfd776
Charles Peach <charles@3months.com>
2013-03-22 21:41:20 +1300

    Add Questions to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20130322073030) do
+
+  create_table "questions", :force => true do |t|
+    t.string   "title",      :null => false
+    t.text     "content",    :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
