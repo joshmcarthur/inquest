3e0f69c304c252ef6df8dc5c90afb48f78b75b35
Josh McArthur <joshua.mcarthur@gmail.com>
2013-03-24 12:02:12 +1300

    Spec accept_answer_link method

--- spec/helpers/answers_helper_spec.rb
+++ spec/helpers/answers_helper_spec.rb
@@ -11,4 +11,56 @@ require 'spec_helper'
 #   end
 # end
 describe AnswersHelper do
+  fixtures :questions, :users, :answers
+
+  let(:question) { questions(:test) }
+
+  describe "#accept_answer_link" do
+    context "question is owned by current user and is not accepted" do
+      before do
+        helper.stub(:current_user).and_return(users(:tester))
+        @output = helper.accept_answer_link(answers(:test))
+      end
+
+      it "should have a link to accept the answer" do
+        @output.should include accept_question_answer_path(question, answers(:test))
+      end
+
+      it "should render the accepted icon" do
+        @output.should include "class=\"icon-ok-sign icon-2x\""
+      end
+    end
+
+    context "question already has accepted answer" do
+      before do
+        helper.stub(:current_user).and_return(users(:tester))
+        answers(:test).accept!
+        @output = helper.accept_answer_link(answers(:another_test))
+      end
+
+      it "should not have a link to accept the answer" do
+        @output.should_not include accept_question_answer_path(question, answers(:another_test))
+      end
+
+      it "should not render the accepted icon" do
+        @output.should_not include "class=\"icon-ok-sign icon-2x\""
+      end
+    end
+
+    context "question is not owned by the current user but is accepted" do
+      before do
+        helper.stub(:current_user).and_return(users(:answerer))
+        answers(:test).accept!
+        @output = helper.accept_answer_link(answers(:test))
+      end
+
+      it "should not have a link to accept the answer" do
+        @output.should_not include accept_question_answer_path(question, answers(:test))
+      end
+
+      it "should render the accepted icon" do
+        @output.should include "class=\"icon-ok-sign icon-2x\""
+      end
+    end
+  end
 end
