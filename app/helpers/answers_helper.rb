5abab69fa8a51b84960c9cbe40593f85a04888a2
Josh McArthur <joshua.mcarthur@gmail.com>
2013-03-24 12:35:11 +1300

    Update the method that is called to check if the answer is acceptable

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,7 +1,7 @@
 module AnswersHelper
 
   def accept_answer_link(answer)
-    if answer.question.owned_by?(current_user) && answer.answerable?
+    if answer.question.owned_by?(current_user) && answer.acceptable?
       link_to accept_question_answer_path(answer.question, answer), :id => 'accept_answer', :title => 'Mark this question as accepted.', :method => :put do
         glyph('ok-sign', 3)
       end
