d9264dc1450cdde149140a16df03cc874869dd5f
Josh McArthur <joshua.mcarthur@gmail.com>
2013-03-24 12:34:32 +1300

    Remove method that has been superceded by Answer#acceptable?, and add some helper methods to render markdown content into HTMl

--- app/models/question.rb
+++ app/models/question.rb
@@ -8,10 +8,23 @@ class Question < ActiveRecord::Base
   validates :title, :presence => true
   validates :content, :presence => true
 
-  def answerable?
-    self.accepted_answer.nil?
-  end
 
+  # Public: Render the Markdown content of this question as HTML.
+  #
+  # This method uses the Redcarpet library to convert the Markdown
+  # question content into HTML for rendering into a view.
+  #
+  # #FIXME it should also handle sanitizing the output, stripping potentially
+  # dangerous or disallowed tags.
+  #
+  # Returns the generated HTML for the question content
+  def content_html
+    Markdown
+
+  # Public: Determine whether this question is owned by the given user.
+  #
+  # Simple shortcut to tidy up views.
+  # Returns true if the question is owned by the user, false otherwise.
   def owned_by?(user)
     self.user == user
   end
