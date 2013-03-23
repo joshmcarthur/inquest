7c011dabdde7c9b8aa66fd1d415da17468e5d530
Charles Peach <charles@3months.com>
2013-03-24 00:22:21 +1300

    Add questions controller spec

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -5,7 +5,7 @@ describe QuestionsController do
 
   context "when I am not logged in" do
     it "should require an authenticated user" do
-      get :index 
+      get :index
       response.should redirect_to new_user_session_path
       flash[:alert].should_not be_blank
     end
