afdb611d6120d11ed2e708d6ce891620a77fc1eb
Josh McArthur <joshua.mcarthur@gmail.com>
2013-03-24 15:47:10 +1300

    Set action mailer host to emails get delivered

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,9 @@ Inquest::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # MailCatcher config for development environment
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  config.action_mailer.default_url_options = { :host => 'aviation.local:3000' }
 end
