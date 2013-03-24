d565c896a41b5cfd92dd8a8c3d7f0779282ce69b
Josh McArthur <joshua.mcarthur@gmail.com>
2013-03-24 14:29:48 +1300

    Spec votes helper

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,2 +1,27 @@
 module VotesHelper
+
+  def vote_button(voteable, direction)
+    icon = glyph("arrow-#{direction}", 3)
+    path = if voteable.is_a?(Answer)
+      question_answer_votes_path(voteable.question, voteable, :direction => direction)
+    else
+      question_votes_path(voteable, :direction => direction)
+    end
+
+    if current_user.voted_on?(voteable, direction)
+      icon
+    elsif !current_user.voted_on?(voteable)
+      link_to path, :method => :post, :class => 'vote_button' do
+        icon
+      end
+    else
+      content_tag :span, :class => 'vote_button disabled' do
+        icon
+      end
+    end
+  end
+
+  def vote_score(voteable) 
+    content_tag(:h4, voteable.vote_score, :class => 'vote-score')
+  end
 end
