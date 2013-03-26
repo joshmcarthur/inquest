66828c6b4393f85e846807f2d4b1c41edf0bd339
Josh McArthur <joshua.mcarthur@gmail.com>
2013-03-26 23:53:07 +1300

    Track create events on comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,9 @@
 class Comment < ActiveRecord::Base
   include ActiveModel::ForbiddenAttributesProtection
+  include PublicActivity::Model
+
+  tracked :owner => ->(controller, model) { controller && controller.send(:current_user) }
+  
   belongs_to :commentable, :polymorphic => true
   belongs_to :user
 
