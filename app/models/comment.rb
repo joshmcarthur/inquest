f597d81e7b656089e44ad6084a31cf83c9278987
Josh McArthur <joshua.mcarthur@gmail.com>
2013-03-26 23:28:44 +1300

    Validate presence of comment content

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,5 @@ class Comment < ActiveRecord::Base
   belongs_to :commentable, :polymorphic => true
   belongs_to :user
 
-  validates :commentable, :user, :presence => true
+  validates :commentable, :user, :content, :presence => true
 end
