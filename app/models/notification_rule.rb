3cb9eb492bc8b604a00374f01325bee0abe99c2c
Josh McArthur <joshua.mcarthur@gmail.com>
2013-03-27 22:10:36 +1300

    Complete testing notification rule model

--- app/models/notification_rule.rb
+++ app/models/notification_rule.rb
@@ -1,9 +1,15 @@
 class NotificationRule < ActiveRecord::Base
   include ActiveModel::ForbiddenAttributesProtection
-  ACTIONS = [:create, :update]
+  ACTIONS = %w( create update )
 
   belongs_to :user
-  validates :class_name, :inclusion => {:in => Inquest::Application.config.notifiable_classes }
-  validates :reactor_name, :inclusion => {:in => Reactor.subclasses.map(&:name) } 
+  validates :class_name, :inclusion => {:in => Inquest::Application.config.notifiable_classes.map { |c| c.name } }
+  validates :reactor_name, :inclusion => {:in => proc { Inquest::Reactor.subclasses.map { |r| r.name } } } 
   validates :action, :inclusion => {:in => ACTIONS}
+
+  def reactor
+    Inquest::Reactor.subclasses.select do |sc|
+      sc.name == self.reactor_name.to_s 
+    end.first
+  end
 end
--- spec/models/notification_rule_spec.rb
+++ spec/models/notification_rule_spec.rb
@@ -12,17 +12,17 @@ describe NotificationRule do
   end
 
   it "should not be valid when a vital attribute is missing" do
-    subject.reactor_class = nil
+    subject.reactor_name = nil
     subject.should_not be_valid
   end
 
   it "should not be valid when an invalid reactor class is present" do
-    subject.reactor_class = "Unknown"
+    subject.reactor_name = "Unknown"
     subject.should_not be_valid
   end
 
   it "should find the reactor class when a valid reactor name is provided" do
-    subject.reactor_class = "EmailReactor"
-    subject.reactor.should be_a EmailReactor
+    subject.reactor_name = "EmailReactor"
+    subject.reactor.should eq EmailReactor
   end
 end
