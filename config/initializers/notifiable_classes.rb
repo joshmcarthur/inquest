1e6042afe0ca99f996b4e39023fdb6bdb65cdc95
Josh McArthur <joshua.mcarthur@gmail.com>
2013-03-27 23:20:55 +1300

    Check that the notifiable classes include the Inquest::Notifiable module, and warn if they do not. Record reactor classes in config, as trying to automatically determine them is not working correctly.

--- config/initializers/notifiable_classes.rb
+++ config/initializers/notifiable_classes.rb
@@ -1,3 +1,14 @@
 # Set classes that can be notified when
 # actions happen to them
-Inquest::Application.config.notifiable_classes = [Question, Answer, Comment, Vote]
+Inquest::Application.config.notifiable_classes = [Question, Answer]
+
+# Include the notifiable module to make sure they conform to what a notifiable model must do
+Inquest::Application.config.notifiable_classes.each do |klass|
+  unless klass.included_modules.include?(Inquest::Notifiable)
+    Rails.logger.warn "#{klass.name} does not include Inquest::Notifiable, so may not work correctly."
+  end
+end
+
+# Set classes that can react to events on notifiable classes.
+# These classes should inherit from Inquest::Reactor
+Inquest::Application.config.reactor_classes = [EmailReactor]
