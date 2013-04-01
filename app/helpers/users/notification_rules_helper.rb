ffcd565169b655b203da61da7cc45af0aeb218d1
Josh McArthur <joshua.mcarthur@gmail.com>
2013-04-01 20:51:21 +1300

    Update options for notification rule helper to use Disclosure collections

--- app/helpers/users/notification_rules_helper.rb
+++ app/helpers/users/notification_rules_helper.rb
@@ -1,11 +1,5 @@
 module Users::NotificationRulesHelper
   def class_name_options_for_select
-    Inquest::Application.config.notifiable_classes.map { |nc| [nc.name.pluralize, nc.name] }
+    Disclosure.configuration.notifier_classes.map { |nc| [nc.name.pluralize, nc.name] }
   end
-
-  def action_options_for_select
-    NotificationRule::ACTIONS.map { |a| ["#{a}d", a] }
-  end
-
-  
 end
