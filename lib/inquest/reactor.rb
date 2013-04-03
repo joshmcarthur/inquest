9e7f203b169799ad3fb6f52d32f8fd4188b15601
Josh McArthur <joshua.mcarthur@gmail.com>
2013-04-03 20:23:16 +1300

    Remove files that were originally intended for what is now disclosure

--- lib/inquest/reactor.rb
+++
@@ -1,9 +0,0 @@
-class Inquest::Reactor
-  def self.react!(user, notifiable, action)
-    raise NotYetImplementedError
-  end
-
-  def self.reaction_description
-    raise NotYetImplementedError
-  end
-end
