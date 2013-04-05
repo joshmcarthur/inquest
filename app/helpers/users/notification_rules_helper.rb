41719ad94ce4e4a079bbb91edc2336110fe28b19
Josh McArthur <joshua.mcarthur@gmail.com>
2013-04-06 00:18:32 +1300

    Tidy up notification rules form and display

--- app/helpers/users/notification_rules_helper.rb
+++ app/helpers/users/notification_rules_helper.rb
@@ -1,5 +1,9 @@
 module Users::NotificationRulesHelper
   def class_name_options_for_select
-    Disclosure.configuration.notifier_classes.map { |nc| [nc.name.pluralize, nc.name] }
+    [Question, Answer].map { |nc| [nc.name.pluralize, nc.name] }
+  end
+
+  def action_options_for_select(klass)
+    klass.notifiable_actions.map { |action| [action.gsub(/[e|ed]*\Z/, '') + 'ed', action] }
   end
 end
