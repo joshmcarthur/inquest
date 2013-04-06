dc3cbfa248948063335a841749783d90488d1940
Josh McArthur <joshua.mcarthur@gmail.com>
2013-04-07 00:25:22 +1300

    Add ability to pass additional classes into glyph helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
-  def glyph(icon, size = 1)
-    content_tag(:i, '', :class => "icon-#{icon} #{size > 1 ? "icon-#{size}x" : nil}")
+  def glyph(icon, size = 1, classes = "")
+    content_tag(:i, '', :class => "icon-#{icon} #{size > 1 ? "icon-#{size}x" : nil} #{classes}".strip)
   end
 
   def inline_user_display(user) 
