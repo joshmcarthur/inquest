0230a87c1e138f38914a07bf50613935890cb244
Josh McArthur <joshua.mcarthur@gmail.com>
2013-04-06 21:32:09 +1300

    Add ability to be notified of new comments

--- app/helpers/users/notification_rules_helper.rb
+++ app/helpers/users/notification_rules_helper.rb
@@ -1,6 +1,6 @@
 module Users::NotificationRulesHelper
   def class_name_options_for_select
-    [Question, Answer].map { |nc| [nc.name.pluralize, nc.name] }
+    [Question, Answer, Comment, Vote].map { |nc| [nc.name.pluralize, nc.name] }
   end
 
   def action_options_for_select(klass)
