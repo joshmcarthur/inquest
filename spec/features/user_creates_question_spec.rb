9da5612750dadfceafdfc39b73e3cad068f51b57
Josh McArthur <joshua.mcarthur@gmail.com>
2013-04-15 23:57:51 +1200

    Spec successful question create flow

---
+++ spec/features/user_creates_question_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe "User creates question" do
+  fixtures :users, :questions, :tags
+  let(:user) { users(:tester) }
+  let(:question) { questions(:test) }
+
+  describe "creating question" do
+
+    before(:each) do
+      tags
+      sign_in_as user
+      click_link 'New Question'
+    end
+
+    describe "with valid inputs" do
+      before do
+        within "form#new_question" do
+          fill_in "Title", :with => question.title
+          select tags(:test).name, :from => 'question[tag_ids][]'
+          fill_in 'Content', :with => question.content
+          click_button 'Create Question'
+        end
+      end
+
+      it "should be on the question page" do
+        current_path.should match question_path(Question.last.id)
+      end
+
+      it "should show the title" do
+        page.should have_content question.title
+      end
+
+      it "should show tags" do
+        all('.tags-display > span.tag').should have(1).item
+      end
+
+      it "should show content" do
+        page.should have_content question.content
+      end
+
+      it "should show the owner controls" do
+        page.should have_selector "#owner_controls"
+      end
+    end
+  end
+end
