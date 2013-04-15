3c799574cc4bcb702c5f9b6bbf42a1cbe646e5fa
Josh McArthur <joshua.mcarthur@gmail.com>
2013-04-16 00:05:57 +1200

    Add some in-progress specs to tag model

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Tag do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :tags
+
+  it "should be valid" do
+    subject.name = "New tag"
+    subject.should be_valid
+  end
+
+  # it "should require a name" do
+  #   subject.name = ""
+  #   subject.valid?
+  #   subject.errors[:name].should_not be_blank
+  # end
+
+  # it "should require a unique name" do
+  #   subject.name = tags(:test).name
+  #   subject.valid?
+  #   subject.errors[:name].should_not be_blank
+  # end
 end
