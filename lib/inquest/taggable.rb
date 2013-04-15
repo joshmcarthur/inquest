e929f239d3994fb90f02533238d17f003aa16048
Josh McArthur <joshua.mcarthur@gmail.com>
2013-04-15 21:39:09 +1200

    Prevent duplicate tags from being added

--- lib/inquest/taggable.rb
+++ lib/inquest/taggable.rb
@@ -6,10 +6,14 @@ module Inquest
 
         def tags_list=(new_tags)
           new_tags.split(',').each do |tag_name|
-            if tag = Tag.where(:name => tag_name.strip).first
+            if (tag = Tag.where(:name => tag_name.strip).first) && !tagged_with?(tag)
               self.tags << tag
             end
           end
+        end
+
+        def tagged_with?(tag)
+          self.tags.where(:id => tag.id).any?
         end
 
         def tags_list
