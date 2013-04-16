32ff8f8a4d197784fb18a06e0b2bb324f8f740a4
Josh McArthur <joshua.mcarthur@gmail.com>
2013-04-16 21:03:23 +1200

    Add inverse of questions tags association to Tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ActiveRecord::Base
   include ActiveModel::ForbiddenAttributesProtection
   validate :name, uniqueness: true, allow_blank: false
+
+  has_and_belongs_to_many :questions
 end
